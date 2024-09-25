7b172f4bec808f21790cc0ac81914f3c88f20219
Josh <joshuagarza42@gmail.com>
2024-09-25 07:12:58 -0700

    Adding header, glossary terms and practice template

---
+++ python_code_header.py
@@ -0,0 +1,16 @@
+#******************************************************************************
+# Author:           (programmer's name)
+# Lab:              (fill in with Lab 1, etc.)
+# Date:             (fill in)
+# Description:      Example: This program prompts a user for their name and
+#                   prints a message to the screen.
+# Input:            (What the program asks for, and the data type, e.g. string)
+# Output:           (Summary of messages displayed by the program)
+# Sources:          Lab 1 specifications
+#                   and any other substantial aids, like web pages or students
+#                   who helped you.
+#******************************************************************************
+
+# Neither comments nor code should be wider than 79 characters.
+# The lines of asterisks above are 79 characters long for easy reference.
+# You don't need to include these three lines in your program!
